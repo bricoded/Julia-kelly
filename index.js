e2fbb70bce8a75ee0550b881ecbd1662d155df70
bricoded <bacarybricoded@gmail.com>
2023-05-16 16:36:25 +0200

    finished homepage

---
+++ index.js
@@ -0,0 +1,25 @@
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('nav ul');
+const icon = document.querySelector('.btn-icon');
+hamburger.addEventListener('click', function () {
+  hamburger.classList.toggle('open');
+  menu.classList.toggle('show');
+  icon.classList.toggle('show');
+  console.log(icon.classList)
+})
+
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+    else {
+      entry.target.classList.remove('show');
+    }
+  })
+})
+
+const hiddenElement = document.querySelectorAll('.hidden');
+hiddenElement.forEach((el) => observer.observe(el))
